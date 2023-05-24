e1cd597c27e05ede20ab59cc0b50e04049493a03
= <polad.ibrahimiv@gmail.com>
2023-05-24 07:52:14 -0700

    Only first entrance page

---
+++ android/app/src/main/kotlin/com/example/flt_fashion/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flt_fashion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/entrance.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class EntrancePage extends StatefulWidget {
+  const EntrancePage({super.key});
+
+  @override
+  State<EntrancePage> createState() => _EntrancePageState();
+}
+
+class _EntrancePageState extends State<EntrancePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(children: [
+          Image.asset(
+            "assets/images/firstImage.jpg",
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.black.withOpacity(0.5),
+          ),
+          Positioned(
+              bottom: 150,
+              right: 50,
+              child: Column(
+                children: [
+                  Text(
+                    "Ideal Store For Shopping",
+                    style: TextStyle(fontSize: 24, color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    height: 60,
+                    width: 300,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+                      },
+                      child: Text(
+                        "Sign In With Email",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                          side: BorderSide(color: Colors.white24), //border width and color
+                          elevation: 3, //elevation of button
+                          shape: RoundedRectangleBorder(
+                              //to set border radius to button
+                              borderRadius: BorderRadius.circular(3)),
+                          padding: EdgeInsets.all(20) //content padding inside button
+                          ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    height: 60,
+                    width: 300,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red, //background color of button
+                          side: BorderSide(width: 3, color: Colors.white24), //border width and color
+                          elevation: 3, //elevation of button
+                          shape: RoundedRectangleBorder(
+                              //to set border radius to button
+                              borderRadius: BorderRadius.circular(3)),
+                          padding: EdgeInsets.all(20) //content padding inside button
+                          ),
+                      child: const Row(
+                        children: [
+                          Icon(
+                            Icons.facebook,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            "Continue With Facebook",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              )),
+          Positioned(
+            right: 0,
+            child: Row(
+              children: [
+                TextButton(
+                  onPressed: () {
+                    // Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpPage()));
+                  },
+                  child: Text(
+                    "Sign Up",
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                ),
+                Icon(Icons.arrow_forward_ios, color: Colors.white),
+              ],
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
